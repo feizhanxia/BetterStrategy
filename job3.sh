6e098dd21454769811aa2c24c7c1df4f788b652a
feizhanxia <2050570@tongji.edu.cn>
2024-05-11 17:44:07 +0800

    sbatch train

---
+++ job3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p TH_HPC3N
+#SBATCH -J train9
+#SBATCH -N 1
+#SBATCH -n=1
+#SBATCH -c 32
+#SBATCH -t 5:00:00
+#SBATCH -o output/slurm_output/train9.out
+
+module purge
+module load loginnode
+source activate py3.10
+
+python train9.py
